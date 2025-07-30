4aee20e5ced593a1e99a151b60c0180a9767713d
sergej.ocheretko1@gmail.com <sergej.ocheretko1@gmail.com>
2025-07-30 19:03:27 +0300

    добавил докер файл

--- src/test/java/UiTests/base/UiBaseTest.java
+++ src/test/java/UiTests/base/UiBaseTest.java
@@ -1,42 +1,57 @@
 package UiTests.base;
-
 import UiTests.utils.TestConfig;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.WebDriverRunner;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 import static com.codeborne.selenide.Selenide.open;
 
-public abstract class UiBaseTest {
+public class UiBaseTest {
+
 
     @BeforeEach
-    final void setUp() {
-        String browser = System.getenv().getOrDefault("BROWSER", "chrome");
-        Configuration.remote           = "http://localhost:4444/wd/hub";
-        Configuration.browserSize      = TestConfig.browserSize();
-        Configuration.timeout          = TestConfig.timeout();
-        Configuration.headless         = TestConfig.headless();
-        Configuration.pageLoadTimeout  = TestConfig.pageLoadTimeout();
-        Configuration.baseUrl          = TestConfig.baseUrl();
+    void setUp() {
 
-        if ("chrome".equals(browser)) {
-            ChromeOptions co = new ChromeOptions();
-            co.addArguments("--host-resolver-rules=MAP pagead2.googlesyndication.com 127.0.0.1,MAP googleads.g.doubleclick.net 127.0.0.1,MAP tpc.googlesyndication.com 127.0.0.1");
-            co.addArguments("--disable-extensions","--disable-background-networking","--no-sandbox","--disable-dev-shm-usage");
-            Configuration.browserCapabilities = co;
-        } else {
-            Configuration.browserCapabilities = new FirefoxOptions();
-        }
 
-        Configuration.browser = browser;
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--host-resolver-rules=" +
+                "MAP pagead2.googlesyndication.com 127.0.0.1," +
+                "MAP googleads.g.doubleclick.net 127.0.0.1," +
+                "MAP tpc.googlesyndication.com 127.0.0.1");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-background-networking");
+        options.addArguments("--disable-default-apps");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+            Configuration.browserCapabilities = options;
+            Configuration.baseUrl     = TestConfig.baseUrl();
+            Configuration.browserSize   = TestConfig.browserSize();
+            Configuration.timeout       = TestConfig.timeout();
+            Configuration.headless      = TestConfig.headless();
+            Configuration.pageLoadTimeout = TestConfig.pageLoadTimeout();
+
+
+
         open(TestConfig.baseUrl());
+            WebDriverRunner.getWebDriver().manage().window().maximize();
     }
-
     @AfterEach
-    final void tearDown() {
+    void tearDown() {
         Selenide.closeWebDriver();
     }
 }
+
+
+
+
+
+
